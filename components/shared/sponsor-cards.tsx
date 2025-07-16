0eca5f4c49513ae130220106b040d71a4e261742
Drdruide <epices.agrume-0@icloud.com>
2025-07-16 10:58:25 +0200

    chore: new sponsor

--- components/shared/sponsor-cards.tsx
+++ components/shared/sponsor-cards.tsx
@@ -9,6 +9,12 @@ export function SponsorCards() {
         imageUrl="https://yxucdfr9f5.ufs.sh/f/M3RHr0TmpHk5q9Vu6GFecLtRGiOZFW9SkwX8Df0YhJlsyPUm"
         description="A Flexible, Modern Full-Stack SaaS Template"
       />
+      <SponsorCard
+        name="Findly.tools"
+        url="https://findly.tools?ref=open-launch"
+        imageUrl="https://yxucdfr9f5.ufs.sh/f/M3RHr0TmpHk58YQSMZbg1XPzV7Kxo25HAvNtwa6hLcRpjB0T"
+        description="The best tools, all in one place."
+      />
     </>
   )
 }
